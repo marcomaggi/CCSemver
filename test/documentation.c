03c2d7a02972146035c2b0d36ab76dcdf638aae8
Marco Maggi <marco.maggi-ipsu@poste.it>
2017-05-20 20:47:22 +0200

    development of texinfo documentation

--- test/documentation.c
+++ test/documentation.c
@@ -96,6 +96,26 @@ semver_comp_fwrite (const semver_comp_t * compp, FILE * stream)
   }
 }
 
+static int
+semver_range_fwrite (const semver_range_t * rangep, FILE * stream)
+{
+  size_t	buffer_len = 64;
+  char		buffer_ptr[buffer_len];
+  size_t	needed_count;
+
+  needed_count = (size_t)semver_range_pwrite(rangep, buffer_ptr, buffer_len);
+  if (0 == needed_count) {
+    return 0;
+  } else if (needed_count < buffer_len) {
+    return fwrite(buffer_ptr, sizeof(char), needed_count, stream);
+  } else {
+    size_t	buffer_len = needed_count+1;
+    char	buffer_ptr[buffer_len];
+    size_t	actual_count = semver_range_pwrite(rangep, buffer_ptr, buffer_len);
+    return fwrite(buffer_ptr, sizeof(char), actual_count, stdout);
+  }
+}
+
 const char *
 semver_op_string (enum semver_op op)
 {
@@ -1118,34 +1138,158 @@ doc_example_comparators_4_5_2 (void)
 void
 doc_example_ranges_1_1 (void)
 {
-#if 0
+  static const char	input_str[] = "1.2.3";
   semver_range_t	range;
+  size_t		offset = 0;
+  char			rv;
 
-  semver_range_ctor(&range);
-  {
+  rv = semver_range_read(&range, input_str, strlen(input_str), &offset);
+  if (0 == rv) {
     /* Do something with "range" here. */
   }
   semver_range_dtor(&range);
-#endif
 }
 
 void
 doc_example_ranges_1_2 (void)
 {
-#if 0
+  static const char	input_str[] = "1.2.3";
   semver_range_t *	rangep;
+  size_t		offset = 0;
+  char			rv;
 
-  rangep = malloc(sizeof(semver_t));
+  rangep = (semver_range_t *) malloc(sizeof(semver_range_t));
   assert(NULL != rangep);
   {
-    semver_range_ctor(rangep);
-    {
+    rv = semver_range_read(rangep, input_str, strlen(input_str), &offset);
+    if (0 == rv) {
       /* Do something with "rangep" here. */
     }
     semver_range_dtor(rangep);
   }
   free(rangep);
-#endif
+}
+
+/* ------------------------------------------------------------------ */
+
+void
+doc_example_ranges_2_1 (void)
+/* Reading a simple range. */
+{
+  printf("--- %s:\n", __func__);
+  static const char	input_str[] = "1.2.3";
+  semver_range_t	range;
+  size_t		offset = 0;
+  char			rv;
+
+  rv = semver_range_read(&range, input_str, strlen(input_str), &offset);
+  if (0 == rv) {
+    printf("range: ");
+    semver_comp_fwrite(&range.comp, stdout);
+    printf("\n");
+  }
+  semver_range_dtor(&range);
+}
+
+void
+doc_example_ranges_2_2 (void)
+/* Reading a complex range. */
+{
+  printf("--- %s:\n", __func__);
+  static const char	input_str[] = "1.2.7 || >=1.2.9 <2.0.0";
+  semver_range_t	range;
+  size_t		offset = 0;
+  char			rv;
+
+  rv = semver_range_read(&range, input_str, strlen(input_str), &offset);
+  if (0 == rv) {
+    printf("range: ");
+    semver_range_fwrite(&range, stdout);
+    printf("\n");
+  }
+  semver_range_dtor(&range);
+}
+
+/* ------------------------------------------------------------------ */
+
+void
+doc_example_ranges_3_1 (void)
+/* Writing a range, enough room in the buffer. */
+{
+  printf("--- %s:\n", __func__);
+  static const char	input_str[] = "1.2.7 || >=1.2.9 <2.0.0";
+  size_t		buffer_len = 32;
+  char			buffer_ptr[buffer_len];
+  semver_range_t	range;
+  size_t		offset = 0;
+  char			rv;
+
+  memset(buffer_ptr, 0, buffer_len);
+
+  rv = semver_range_read(&range, input_str, strlen(input_str), &offset);
+  if (0 == rv) {
+    size_t	needed_count;
+    size_t	actual_count;
+    needed_count = (size_t)semver_range_write(range, buffer_ptr, buffer_len);
+    if (0 < needed_count) {
+      actual_count = (needed_count < buffer_len)? needed_count : buffer_len;
+      printf("len=%lu, actual_count=%lu, range=",
+	     strlen(input_str), actual_count);
+      fwrite(buffer_ptr, sizeof(char), actual_count, stdout);
+      printf("\n");
+    }
+  }
+  semver_range_dtor(&range);
+}
+
+/* ------------------------------------------------------------------ */
+
+void
+doc_example_ranges_4_1 (void)
+/* Matching, matches. */
+{
+  printf("--- %s:\n", __func__);
+  static const char	version_str[] = "1.4.6";
+  static const char	range_str[]  = "1.2.7 || >=1.2.9 <2.0.0";
+  semver_t		version;
+  semver_range_t	range;
+  size_t		version_off = 0, range_off = 0;
+  char			rv;
+
+  rv = semver_read(&version, version_str, strlen(version_str), &version_off);
+  if (0 == rv) {
+    rv = semver_range_read(&range, range_str, strlen(range_str), &range_off);
+    if (0 == rv) {
+      int	result = (int)semver_rmatch(version, range);
+      printf("matches?=%s\n", (result)? "yes" : "no");
+    }
+    semver_range_dtor(&range);
+  }
+  semver_dtor(&version);
+}
+
+void
+doc_example_ranges_4_2 (void)
+/* Matching, does not match. */
+{
+  printf("--- %s:\n", __func__);
+  static const char	version_str[] = "1.2.8";
+  static const char	range_str[]  = "1.2.7 || >=1.2.9 <2.0.0";
+  semver_t		version;
+  semver_range_t	range;
+  size_t		version_off = 0, range_off = 0;
+  char			rv;
+
+  rv = semver_read(&version, version_str, strlen(version_str), &version_off);
+  if (0 == rv) {
+    rv = semver_range_read(&range, range_str, strlen(range_str), &range_off);
+    if (0 == rv) {
+      int	result = (int)semver_rmatch(version, range);
+      printf("matches?=%s\n", (result)? "yes" : "no");
+    }
+    semver_range_dtor(&range);
+  }
+  semver_dtor(&version);
 }
 
 
@@ -1210,6 +1354,11 @@ main (void)
 
   doc_example_ranges_1_1();
   doc_example_ranges_1_2();
+  doc_example_ranges_2_1();
+  doc_example_ranges_2_2();
+  doc_example_ranges_3_1();
+  doc_example_ranges_4_1();
+  doc_example_ranges_4_2();
 
   return EXIT_SUCCESS;
 }
