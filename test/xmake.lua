84c135c2f0f1cacf208efcc4dd04aeae305a47b3
Marco Maggi <marco.maggi-ipsu@poste.it>
2017-05-21 06:34:00 +0200

    added documentation test file to xmake.lua

--- test/xmake.lua
+++ test/xmake.lua
@@ -18,6 +18,9 @@ target("range_test")
 target("match_test")
     add_files("match.c")
 
+target("documentation_test")
+    add_files("documentation.c")
+
 task("check")
     on_run(function ()
         import("core.project.task")
@@ -25,6 +28,7 @@ task("check")
         task.run("run", {target = "comp_test"})
         task.run("run", {target = "range_test"})
         task.run("run", {target = "match_test"})
+        task.run("run", {target = "documentation_test"})
     end)
     set_menu {
         usage = "xmake check"
