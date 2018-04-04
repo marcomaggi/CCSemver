62e02392bea1e8ceca85b9c1cdf6a49004da8933
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-04-04 06:46:18 +0200

    code review

--- src/ccsemver-id.c
+++ src/ccsemver-id.c
@@ -1,30 +1,37 @@
 /*
- * This  is free  and  unencumbered software  released  into the  public
- * domain.
- *
- * Anyone  is free  to copy,  modify,  publish, use,  compile, sell,  or
- * distribute this software, either in source code form or as a compiled
- * binary, for  any purpose,  commercial or  non-commercial, and  by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of  this software  dedicate any  and  all copyright  interest in  the
- * software  to the  public  domain.  We make  this  dedication for  the
- * benefit of the public at large and  to the detriment of our heirs and
- * successors.  We  intend  this  dedication  to  be  an  overt  act  of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE  SOFTWARE IS  PROVIDED "AS  IS",  WITHOUT WARRANTY  OF ANY  KIND,
- * EXPRESS OR  IMPLIED, INCLUDING BUT  NOT LIMITED TO THE  WARRANTIES OF
- * MERCHANTABILITY,    FITNESS   FOR    A    PARTICULAR   PURPOSE    AND
- * NONINFRINGEMENT.  IN  NO EVENT  SHALL THE AUTHORS  BE LIABLE  FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR  OTHERWISE, ARISING FROM,  OUT OF  OR IN CONNECTION  WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <http://unlicense.org>
- */
+  Part of: CCSemver
+  Contents: parser functions for identifiers
+
+  Abstract
+
+	Parser functions for identifiers.
+
+  This  is  free and  unencumbered  software  released into  the  public
+  domain.
+
+  Anyone  is free  to  copy,  modify, publish,  use,  compile, sell,  or
+  distribute this software, either in source  code form or as a compiled
+  binary,  for any  purpose, commercial  or non-commercial,  and by  any
+  means.
+
+  In jurisdictions that recognize copyright  laws, the author or authors
+  of  this software  dedicate  any  and all  copyright  interest in  the
+  software  to the  public  domain.   We make  this  dedication for  the
+  benefit of the public  at large and to the detriment  of our heirs and
+  successors.   We  intend  this  dedication  to  be  an  overt  act  of
+  relinquishment in perpetuity of all  present and future rights to this
+  software under copyright law.
+
+  THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+  EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO  EVENT SHALL  THE AUTHORS  BE LIABLE FOR  ANY CLAIM,  DAMAGES OR
+  OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT, TORT OR OTHERWISE,
+  ARISING FROM, OUT OF OR IN CONNECTION  WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+
+  For more information, please refer to <http://unlicense.org>
+*/
 
 
 /** --------------------------------------------------------------------
@@ -36,10 +43,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
-
-#ifdef _MSC_VER
-# define snprintf(s, maxlen, fmt, ...) _snprintf_s(s, _TRUNCATE, maxlen, fmt, __VA_ARGS__)
-#endif
 
 
 /** --------------------------------------------------------------------
@@ -57,7 +60,7 @@ ccsemver_id_ctor (ccsemver_id_t *self)
 }
 
 void
-ccsemver_id_dtor(ccsemver_id_t *self)
+ccsemver_id_dtor (ccsemver_id_t *self)
 {
   if (self && self->next) {
     ccsemver_id_dtor(self->next);
@@ -65,6 +68,11 @@ ccsemver_id_dtor(ccsemver_id_t *self)
     self->next = NULL;
   }
 }
+
+
+/** --------------------------------------------------------------------
+ ** Parser.
+ ** ----------------------------------------------------------------- */
 
 char
 ccsemver_id_read (ccsemver_id_t *self, const char *str, size_t len, size_t *offset)
@@ -111,6 +119,11 @@ ccsemver_id_read (ccsemver_id_t *self, const char *str, size_t len, size_t *offs
   return 0;
 }
 
+
+/** --------------------------------------------------------------------
+ ** Comparison.
+ ** ----------------------------------------------------------------- */
+
 char
 ccsemver_id_comp (ccsemver_id_t const * self, ccsemver_id_t const * other)
 {
@@ -154,8 +167,13 @@ ccsemver_id_comp (ccsemver_id_t const * self, ccsemver_id_t const * other)
   return ccsemver_id_comp(self->next, other->next);
 }
 
+
+/** --------------------------------------------------------------------
+ ** Serialisation.
+ ** ----------------------------------------------------------------- */
+
 int
-ccsemver_id_write(ccsemver_id_t const * self, char *buffer, size_t len)
+ccsemver_id_write (ccsemver_id_t const * self, char *buffer, size_t len)
 {
   char next[1024];
 
