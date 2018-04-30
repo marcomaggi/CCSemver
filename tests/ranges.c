a5c477ecf6d7efad0e4f26cdb9edbf0cf0d185f1
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-04-30 06:28:23 +0200

    test review

--- tests/ranges.c
+++ tests/ranges.c
@@ -32,114 +32,240 @@
  ** ----------------------------------------------------------------- */
 
 #include <ccsemver.h>
+#include <cctests.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define STRNSIZE(s) (s), sizeof(s)-1
+
+void
+test_new (cce_destination_t upper_L, char const * const expected, char const * const input_str, bool parse_to_the_end)
+{
+  cce_location_t	L[1];
+  cce_clean_handler_t	H[1];
+
+  if (cce_location(L)) {
+    if (1) { fprintf(stderr, "%s: %s\n", __func__, cce_condition_static_message(cce_condition(L))); }
+    cce_run_error_handlers_raise(L, upper_L);
+  } else {
+    ccsemver_input_t	input = ccsemver_input_new(input_str, strlen(input_str), 0);
+    char		buffer[1024];
+    ccsemver_range_t *	range;
+
+    if (0) { fprintf(stderr, "%s: input: len=%lu, str='%s'\n", __func__, input.len, input.str); }
+
+    range = ccsemver_range_new_guarded(L, H, &input);
+
+    /* Have we parsed the whole input? */
+    if (parse_to_the_end) {
+      cctests_assert(L, false == ccsemver_input_more(&input));
+      cctests_assert(L, true  == ccsemver_input_at_end(&input));
+    }
+
+    {
+      int	slen = ccsemver_range_write(range, buffer, 1024);
+
+      if (0) { fprintf(stderr, "%s: serialised: len=%d, range='%s'\n", __func__, slen, buffer); }
+      cctests_assert_equal_int(L, strlen(expected), slen);
+      cctests_assert_ascii(L, expected, buffer, slen);
+    }
+
+    cce_run_cleanup_handlers(L);
+  }
+}
+
+void
+test_init (cce_destination_t upper_L, char const * const expected, char const * const input_str, bool parse_to_the_end)
+{
+  cce_location_t	L[1];
+  cce_clean_handler_t	H[1];
+
+  if (cce_location(L)) {
+    if (0) { fprintf(stderr, "%s: %s\n", __func__, cce_condition_static_message(cce_condition(L))); }
+    cce_run_error_handlers_raise(L, upper_L);
+  } else {
+    ccsemver_input_t	input = ccsemver_input_new(input_str, strlen(input_str), 0);
+    char		buffer[1024];
+    ccsemver_range_t	range[1];
+
+    if (0) { fprintf(stderr, "%s: input: len=%lu, str='%s'\n", __func__, input.len, input.str); }
+
+    ccsemver_range_init_guarded(L, H, range, &input);
+
+    /* Have we parsed the whole input? */
+    if (parse_to_the_end) {
+      cctests_assert(L, false == ccsemver_input_more(&input));
+      cctests_assert(L, true  == ccsemver_input_at_end(&input));
+    }
+
+    {
+      int	slen = ccsemver_range_write(range, buffer, 1024);
+
+      if (0) { fprintf(stderr, "%s: serialised: len=%d, range='%s'\n", __func__, slen, buffer); }
+      cctests_assert_equal_int(L, strlen(expected), slen);
+      cctests_assert_ascii(L, expected, buffer, slen);
+    }
+
+    cce_run_cleanup_handlers(L);
+  }
+}
 
 
-int
-test_read (char const * const expected, char const * const input_str, size_t const input_len)
-{
-  ccsemver_input_t	input = ccsemver_input_new(input_str, input_len, 0);
-  unsigned		slen;
-  char			buffer[1024];
-  ccsemver_range_t	range;
+/** --------------------------------------------------------------------
+ ** Parsing X-Ranges.
+ ** ----------------------------------------------------------------- */
 
-  printf("test: `%.*s`", (int) input.len, input.str);
-  if (ccsemver_range_read(&range, &input)) {
-    puts("\tunable to parse the range specification");
-    return 1;
+#define GOOD_INPUT_NEW(FUNCNAME, PARSE_TO_THE_END, EXPECTED, INPUT_STR)	\
+  void									\
+  FUNCNAME (cce_destination_t upper_L)					\
+  {									\
+    test_new(upper_L, EXPECTED, INPUT_STR, PARSE_TO_THE_END);		\
   }
-  if (ccsemver_input_more(&input)) {
-    puts("\tunable to parse the whole range specification");
-    return 1;
+
+#define GOOD_INPUT_INIT(FUNCNAME, PARSE_TO_THE_END, EXPECTED, INPUT_STR) \
+  void									\
+  FUNCNAME (cce_destination_t upper_L)					\
+  {									\
+    test_init(upper_L, EXPECTED, INPUT_STR, PARSE_TO_THE_END);		\
   }
-  slen = (unsigned) ccsemver_range_write(&range, buffer, 1024);
-  printf(" \t=> \t`%.*s`", slen, buffer);
-  if (memcmp(expected, buffer, (((size_t) slen > input.len)? slen : input.len)) != 0) {
-    printf(" != `%s`\n", expected);
-    ccsemver_range_dtor(&range);
-    return 1;
-  }
-  printf(" == `%s`\n", expected);
-  ccsemver_range_dtor(&range);
-  return 0;
-}
+
+/* ------------------------------------------------------------------ */
+
+GOOD_INPUT_NEW(test_1_1_1,  true,	">=0.0.0 || 1.2.3",			"* || 1.2.3")
+GOOD_INPUT_NEW(test_1_1_2,  true,	">=1.0.0 <2.0.0 || >=2.0.0 <3.0.0",	"1.x || 2.x")
+GOOD_INPUT_NEW(test_1_1_3,  true,	">=1.2.0 <1.3.0 || 3.0.0",		"1.2.x || 3.0.0")
+GOOD_INPUT_NEW(test_1_1_4,  true,	">=1.0.0 <2.0.0 || >=2.0.0 <3.0.0 || >=3.0.0 <4.0.0", "1 || 2 || 3")
+GOOD_INPUT_NEW(test_1_1_5,  true,	">=1.2.0 <1.3.0 || >=5.0.0",		"1.2 || >=5")
+
+/* ------------------------------------------------------------------ */
+
+GOOD_INPUT_INIT(test_1_2_1,  true,	">=0.0.0 || 1.2.3",			"* || 1.2.3")
+GOOD_INPUT_INIT(test_1_2_2,  true,	">=1.0.0 <2.0.0 || >=2.0.0 <3.0.0",	"1.x || 2.x")
+GOOD_INPUT_INIT(test_1_2_3,  true,	">=1.2.0 <1.3.0 || 3.0.0",		"1.2.x || 3.0.0")
+GOOD_INPUT_INIT(test_1_2_4,  true,	">=1.0.0 <2.0.0 || >=2.0.0 <3.0.0 || >=3.0.0 <4.0.0", "1 || 2 || 3")
+GOOD_INPUT_INIT(test_1_2_5,  true,	">=1.2.0 <1.3.0 || >=5.0.0",		"1.2 || >=5")
+
+
+/** --------------------------------------------------------------------
+ ** Parsing hypen ranges.
+ ** ----------------------------------------------------------------- */
+
+GOOD_INPUT_NEW(test_2_1_1,  true,	">=1.2.3 <=2.3.4 || >=5.0.0",	"1.2.3 - 2.3.4 || >=5")
+GOOD_INPUT_NEW(test_2_1_2,  true,	">=1.2.0 <=2.3.4 || >=5.0.0",	"1.2 - 2.3.4 || >=5")
+GOOD_INPUT_NEW(test_2_1_3,  true,	">=1.2.3 <2.4.0 || >=5.0.0",	"1.2.3 - 2.3 || >=5")
+GOOD_INPUT_NEW(test_2_1_4,  true,	">=1.2.3 <3.0.0 || >=5.0.0",	"1.2.3 - 2 || >=5")
+
+/* ------------------------------------------------------------------ */
+
+GOOD_INPUT_INIT(test_2_2_1,  true,	">=1.2.3 <=2.3.4 || >=5.0.0",	"1.2.3 - 2.3.4 || >=5")
+GOOD_INPUT_INIT(test_2_2_2,  true,	">=1.2.0 <=2.3.4 || >=5.0.0",	"1.2 - 2.3.4 || >=5")
+GOOD_INPUT_INIT(test_2_2_3,  true,	">=1.2.3 <2.4.0 || >=5.0.0",	"1.2.3 - 2.3 || >=5")
+GOOD_INPUT_INIT(test_2_2_4,  true,	">=1.2.3 <3.0.0 || >=5.0.0",	"1.2.3 - 2 || >=5")
+
+
+/** --------------------------------------------------------------------
+ ** Parsing tilde ranges.
+ ** ----------------------------------------------------------------- */
+
+GOOD_INPUT_NEW(test_3_1_1,  true,	">=1.2.3 <1.3.0 || >=5.0.0",	"~1.2.3 || >=5")
+GOOD_INPUT_NEW(test_3_1_2,  true,	">=1.2.0 <1.3.0 || >=5.0.0",	"~1.2 || >=5")
+GOOD_INPUT_NEW(test_3_1_3,  true,	">=1.0.0 <2.0.0 || >=5.0.0",	"~1 || >=5")
+GOOD_INPUT_NEW(test_3_1_4,  true,	">=0.2.3 <0.3.0 || >=5.0.0",	"~0.2.3 || >=5")
+GOOD_INPUT_NEW(test_3_1_5,  true,	">=0.2.0 <0.3.0 || >=5.0.0",	"~0.2 || >=5")
+GOOD_INPUT_NEW(test_3_1_6,  true,	">=0.0.0 <1.0.0 || >=5.0.0",	"~0 || >=5")
+
+/* ------------------------------------------------------------------ */
+
+GOOD_INPUT_NEW(test_3_2_1,  true,	">=1.2.3 <1.3.0 || >=5.0.0",	"~1.2.3 || >=5")
+GOOD_INPUT_NEW(test_3_2_2,  true,	">=1.2.0 <1.3.0 || >=5.0.0",	"~1.2 || >=5")
+GOOD_INPUT_NEW(test_3_2_3,  true,	">=1.0.0 <2.0.0 || >=5.0.0",	"~1 || >=5")
+GOOD_INPUT_NEW(test_3_2_4,  true,	">=0.2.3 <0.3.0 || >=5.0.0",	"~0.2.3 || >=5")
+GOOD_INPUT_NEW(test_3_2_5,  true,	">=0.2.0 <0.3.0 || >=5.0.0",	"~0.2 || >=5")
+GOOD_INPUT_NEW(test_3_2_6,  true,	">=0.0.0 <1.0.0 || >=5.0.0",	"~0 || >=5")
+
+
+/** --------------------------------------------------------------------
+ ** Parsing caret ranges.
+ ** ----------------------------------------------------------------- */
+
+GOOD_INPUT_NEW(test_4_1_1,  true,	">=1.2.3 <2.0.0 || >=5.0.0",	"^1.2.3 || >=5")
+GOOD_INPUT_NEW(test_4_1_2,  true,	">=0.2.3 <0.3.0 || >=5.0.0",	"^0.2.3 || >=5")
+GOOD_INPUT_NEW(test_4_1_3,  true,	">=0.0.3 <0.0.4 || >=5.0.0",	"^0.0.3 || >=5")
+
+/* ------------------------------------------------------------------ */
+
+GOOD_INPUT_NEW(test_4_2_1,  true,	">=1.2.3 <2.0.0 || >=5.0.0",	"^1.2.3 || >=5")
+GOOD_INPUT_NEW(test_4_2_2,  true,	">=0.2.3 <0.3.0 || >=5.0.0",	"^0.2.3 || >=5")
+GOOD_INPUT_NEW(test_4_2_3,  true,	">=0.0.3 <0.0.4 || >=5.0.0",	"^0.0.3 || >=5")
 
 
 int
 main (void)
 {
   ccsemver_init();
+  cctests_init("parsing ranges");
+  {
+    cctests_begin_group("parsing x-ranges");
+    {
+      cctests_run(test_1_1_1);
+      cctests_run(test_1_1_2);
+      cctests_run(test_1_1_3);
+      cctests_run(test_1_1_4);
+      cctests_run(test_1_1_5);
 
-  puts("x-range:");
-  if (test_read(">=0.0.0 || 1.2.3", STRNSIZE("* || 1.2.3"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.0.0 <2.0.0 || >=2.0.0 <3.0.0", STRNSIZE("1.x || 2.x"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.2.0 <1.3.0 || 3.0.0", STRNSIZE("1.2.x || 3.0.0"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=0.0.0", STRNSIZE(""))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.0.0 <2.0.0 || >=2.0.0 <3.0.0 || >=3.0.0 <4.0.0", STRNSIZE("1 || 2 || 3"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.2.0 <1.3.0 || >=5.0.0", STRNSIZE("1.2 || >=5"))) {
-    return EXIT_FAILURE;
-  }
+      cctests_run(test_1_2_1);
+      cctests_run(test_1_2_2);
+      cctests_run(test_1_2_3);
+      cctests_run(test_1_2_4);
+      cctests_run(test_1_2_5);
+    }
+    cctests_end_group();
 
-  puts("\nhyphen:");
-  if (test_read(">=1.2.3 <=2.3.4 || >=5.0.0", STRNSIZE("1.2.3 - 2.3.4 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.2.0 <=2.3.4 || >=5.0.0", STRNSIZE("1.2 - 2.3.4 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.2.3 <2.4.0 || >=5.0.0", STRNSIZE("1.2.3 - 2.3 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.2.3 <3.0.0 || >=5.0.0", STRNSIZE("1.2.3 - 2 || >=5"))) {
-    return EXIT_FAILURE;
-  }
+    cctests_begin_group("parsing hyphen ranges");
+    {
+      cctests_run(test_2_1_1);
+      cctests_run(test_2_1_2);
+      cctests_run(test_2_1_3);
+      cctests_run(test_2_1_4);
 
-  puts("\ntidle:");
-  if (test_read(">=1.2.3 <1.3.0 || >=5.0.0", STRNSIZE("~1.2.3 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.2.0 <1.3.0 || >=5.0.0", STRNSIZE("~1.2 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=1.0.0 <2.0.0 || >=5.0.0", STRNSIZE("~1 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=0.2.3 <0.3.0 || >=5.0.0", STRNSIZE("~0.2.3 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=0.2.0 <0.3.0 || >=5.0.0", STRNSIZE("~0.2 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=0.0.0 <1.0.0 || >=5.0.0", STRNSIZE("~0 || >=5"))) {
-    return EXIT_FAILURE;
-  }
+      cctests_run(test_2_2_1);
+      cctests_run(test_2_2_2);
+      cctests_run(test_2_2_3);
+      cctests_run(test_2_2_4);
+    }
+    cctests_end_group();
 
-  puts("\ncaret:");
-  if (test_read(">=1.2.3 <2.0.0 || >=5.0.0", STRNSIZE("^1.2.3 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=0.2.3 <0.3.0 || >=5.0.0", STRNSIZE("^0.2.3 || >=5"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_read(">=0.0.3 <0.0.4 || >=5.0.0", STRNSIZE("^0.0.3 || >=5"))) {
-    return EXIT_FAILURE;
-  }
+    cctests_begin_group("parsing tilde ranges");
+    {
+      cctests_run(test_3_1_1);
+      cctests_run(test_3_1_2);
+      cctests_run(test_3_1_3);
+      cctests_run(test_3_1_4);
+      cctests_run(test_3_1_5);
+      cctests_run(test_3_1_6);
 
-  return EXIT_SUCCESS;
+      cctests_run(test_3_2_1);
+      cctests_run(test_3_2_2);
+      cctests_run(test_3_2_3);
+      cctests_run(test_3_2_4);
+      cctests_run(test_3_2_5);
+      cctests_run(test_3_2_6);
+    }
+    cctests_end_group();
+
+    cctests_begin_group("parsing caret ranges");
+    {
+      cctests_run(test_4_1_1);
+      cctests_run(test_4_1_2);
+      cctests_run(test_4_1_3);
+
+      cctests_run(test_4_2_1);
+      cctests_run(test_4_2_2);
+      cctests_run(test_4_2_3);
+    }
+    cctests_end_group();
+  }
+  cctests_final();
 }
 
 /* end of file */
