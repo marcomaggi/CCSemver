f962fd6606c2f926fc9e464e4817c8d3623865f2
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-04-30 06:48:24 +0200

    test review

--- tests/match.c
+++ tests/match.c
@@ -32,89 +32,93 @@
  ** ----------------------------------------------------------------- */
 
 #include <ccsemver.h>
+#include <cctests.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-#define STRNSIZE(s) (s), sizeof(s)-1
+#include <string.h>
 
 
-int
-test_match (char expected,
-	    char const * const sv_str, size_t const sv_len,
-	    char const * const cmp_str,  size_t const cmp_len)
+void
+test_match (cce_destination_t upper_L, bool expected, char const * const sv_str,  char const * const cmp_str, bool parse_to_the_end)
 {
-  ccsemver_input_t	sv_input  = ccsemver_input_new(sv_str,  sv_len,  0);
-  ccsemver_input_t	cmp_input = ccsemver_input_new(cmp_str, cmp_len, 0);
-  char			result;
-  ccsemver_t		sv;
-  ccsemver_cmp_t	cmp;
+  cce_location_t	L[1];
+  cce_clean_handler_t	sv_H[1];
+  cce_clean_handler_t	cmp_H[1];
 
-  printf("test: `%.*s` ^ `%.*s`", (int) sv_len, sv_str, (int) cmp_len, cmp_str);
-  if (ccsemver_read(&sv, &sv_input)) {
-    puts(" \tcouldn't parse semantic version specification");
-    return 1;
-  }
-  if (ccsemver_input_more(&sv_input)) {
-    puts("\tunable to parse the whole semantic version specification");
-    return 1;
-  }
+  if (cce_location(L)) {
+    if (1) { fprintf(stderr, "%s: %s\n", __func__, cce_condition_static_message(cce_condition(L))); }
+    cce_run_error_handlers_raise(L, upper_L);
+  } else {
+    ccsemver_input_t	sv_input  = ccsemver_input_new(sv_str,  strlen(sv_str),  0);
+    ccsemver_input_t	cmp_input = ccsemver_input_new(cmp_str, strlen(cmp_str), 0);
+    ccsemver_sv_t *	sv;
+    ccsemver_cmp_t *	cmp;
 
-  if (ccsemver_cmp_read(&cmp, &cmp_input)) {
-    puts("\tcouldn't parse comparator specification");
-    return 1;
-  }
-  if (ccsemver_input_more(&cmp_input)) {
-    puts("\tunable to parse the whole comparator specification");
-    return 1;
-  }
+    if (0) { fprintf(stderr, "%s: sv: len=%lu, str='%s'\n", __func__, sv_input.len, sv_input.str); }
+    if (0) { fprintf(stderr, "%s: cmp: len=%lu, str='%s'\n", __func__, cmp_input.len, cmp_input.str); }
 
-  result = ccsemver_match(&sv, &cmp);
-  printf(" \t=> %d\t", result);
-  {
-    int		rv;
-
-    if (result != expected) {
-      printf(" != `%d`\n", expected);
-      rv = 1;
-    } else {
-      printf(" == `%d`\n", expected);
-      rv = 0;
+    sv	= ccsemver_sv_new_guarded(L, sv_H, &sv_input);
+    /* Have we parsed the whole input? */
+    if (parse_to_the_end) {
+      cctests_assert(L, false == ccsemver_input_more(&sv_input));
+      cctests_assert(L, true  == ccsemver_input_at_end(&sv_input));
     }
-    ccsemver_dtor(&sv);
-    ccsemver_cmp_dtor(&cmp);
-    return rv;
+
+    cmp	= ccsemver_cmp_new_guarded(L, cmp_H, &cmp_input);
+    /* Have we parsed the whole input? */
+    if (parse_to_the_end) {
+      cctests_assert(L, false == ccsemver_input_more(&cmp_input));
+      cctests_assert(L, true  == ccsemver_input_at_end(&cmp_input));
+    }
+
+    cctests_assert(L, expected == ccsemver_match(sv, cmp));
+
+    cce_run_cleanup_handlers(L);
   }
 }
+
+
+/** --------------------------------------------------------------------
+ ** Successfully matching ranges.
+ ** ----------------------------------------------------------------- */
+
+#define GOOD_INPUT(FUNCNAME, PARSE_TO_THE_END, EXPECTED, SV_STR, CMP_STR) \
+  void									\
+  FUNCNAME (cce_destination_t upper_L)					\
+  {									\
+    test_match(upper_L, EXPECTED, SV_STR, CMP_STR, PARSE_TO_THE_END); \
+  }
+
+/* ------------------------------------------------------------------ */
+
+GOOD_INPUT(test_1_1, true,	true, "v1.2.3", "1.2.3")
+GOOD_INPUT(test_1_2, true,	true, "v1.2.3", "1.2.x")
+GOOD_INPUT(test_1_3, true,	true, "v1.2.3", "1.x.x")
+GOOD_INPUT(test_1_4, true,	true, "v1.2.3", "1.x")
+GOOD_INPUT(test_1_5, true,	true, "v1.2.3", "1")
+GOOD_INPUT(test_1_6, true,	true, "v1.2.3", "*")
+GOOD_INPUT(test_1_7, true,	true, "v1.2.3", ">1")
 
 
 int
 main (void)
 {
   ccsemver_init();
+  cctests_init("matching ranges and semvers");
 
-  if (test_match(1, STRNSIZE("v1.2.3"), STRNSIZE("1.2.3"))) {
-    return EXIT_FAILURE;
+  cctests_begin_group("successful matching");
+  {
+    cctests_run(test_1_1);
+    cctests_run(test_1_2);
+    cctests_run(test_1_3);
+    cctests_run(test_1_4);
+    cctests_run(test_1_5);
+    cctests_run(test_1_6);
+    cctests_run(test_1_7);
   }
-  if (test_match(1, STRNSIZE("v1.2.3"), STRNSIZE("1.2.x"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_match(1, STRNSIZE("v1.2.3"), STRNSIZE("1.x.x"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_match(1, STRNSIZE("v1.2.3"), STRNSIZE("1.x"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_match(1, STRNSIZE("v1.2.3"), STRNSIZE("1"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_match(1, STRNSIZE("v1.2.3"), STRNSIZE("*"))) {
-    return EXIT_FAILURE;
-  }
-  if (test_match(1, STRNSIZE("v1.2.3"), STRNSIZE(">1"))) {
-    return EXIT_FAILURE;
-  }
+  cctests_end_group();
 
-  return EXIT_SUCCESS;
+  cctests_final();
 }
 
 /* end of file */
